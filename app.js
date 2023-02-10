015adc1be40f709d46856d39e7b68cc9d1bd796b
ashishthomaschempolil <ashishthomas7@gmail.com>
2023-02-09 20:35:37 -0500

    fix: First draft done

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const myLibrary = []
+const myLibrary = {}
+const addButton = document.getElementById('add-card-initial')
 
+// Books
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 295)
+const theLordOfTheRings = new Book('The Lord of the Rings', 'J.R.R. Tolkien', 1216, 1216)
+const theSilmarillion = new Book('The Silmarillion', 'J.R.R. Tolkien', 480, 480)
+
+// Book constructor
 function Book (title, author, totalPages, nPagesRead) {
   this.title = title
   this.author = author
@@ -7,58 +14,87 @@ function Book (title, author, totalPages, nPagesRead) {
   this.nPagesRead = nPagesRead
 }
 
-function addBookToLibrary (book) {
-  myLibrary.push(book)
-  render()
-}
-
-function createBookCard (book) {
+// Book prototype methods
+Book.prototype.createBookCard = function createBookCard () {
   const bookDiv = document.createElement('div')
   bookDiv.classList.add('book-card')
   bookDiv.innerHTML = `
-    <div class="book-card__title"><h2>${book.title}</h2></div>
-    <div class="book-card__author"><h3>by ${book.author}</h3></div>
-    <div class="book-card__pages"><p>${book.nPagesRead} / ${book.totalPages} pages read</p></div>
-    <div class="remove-button"><button>Remove</button></div>
-    `
+      <div class="book-card__title"><h2>${this.title}</h2></div>
+      <div class="book-card__author"><h3>by ${this.author}</h3></div>
+      <div class="book-card__pages"><p>${this.nPagesRead} / ${this.totalPages} pages read</p></div>
+      <div class="remove-button"><button>Remove</button></div>
+      `
   return bookDiv
 }
 
+// functions
+function addBookToLibrary (book) {
+  myLibrary[book.title] = book
+  render()
+}
+
 function removeBook (e) {
-  if (e.target.classList.contains('remove-button')) {
-    const bookCard = e.target.parentElement
-    bookCard.remove()
-  }
+  console.log(e.target)
+  const bookCard = e.target.parentElement.parentElement // get parent element
+  const title = bookCard.querySelector('.book-card__title h2').textContent
+  delete myLibrary[title]
   render()
 }
 
 function render () {
   const library = document.querySelector('.content')
   library.innerHTML = ''
-  myLibrary.forEach(book => {
-    const bookDiv = createBookCard(book)
-    library.appendChild(bookDiv)
+  for (const book in myLibrary) {
+    const bookCard = myLibrary[book].createBookCard()
+    library.appendChild(bookCard)
+  }
+  library.appendChild(addButton)
+
+  // add event listeners to remove buttons (the updated ones)
+  const removeButton = document.querySelectorAll('.remove-button')
+  removeButton.forEach(button => {
+    button.addEventListener('click', removeBook)
   })
 }
 
-// const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 295)
-// const theLordOfTheRings = new Book('The Lord of the Rings', 'J.R.R. Tolkien', 1216, 1216)
-// const theSilmarillion = new Book('The Silmarillion', 'J.R.R. Tolkien', 480, 480)
+// add books to library
+function addBookFromForm (e) {
+  e.preventDefault()
+  const title = document.getElementById('title').value
+  const author = document.getElementById('author').value
+  const totalPages = document.getElementById('total-pages').value
+  const nPagesRead = document.getElementById('pages-read').value || 0
+  const book = new Book(title, author, totalPages, nPagesRead)
+  addBookToLibrary(book)
 
-// // addBookToLibrary(theLordOfTheRings)
-// // addBookToLibrary(theSilmarillion)
+  // reset form
+  const form = document.getElementById('add-card-form')
+  form.style.display = 'none'
+  const addButton = document.querySelector('.add-card')
+  addButton.style.display = 'flex'
+  form.reset()
 
-// // render()
+  render()
+}
 
-// const library = document.querySelector('.content')
-// library.addEventListener('click', removeBook)
+addBookToLibrary(theHobbit)
+addBookToLibrary(theLordOfTheRings)
+addBookToLibrary(theSilmarillion)
 
-// console.log(myLibrary)
-
-const addButton = document.getElementById('add-card-initial')
 addButton.addEventListener('click', () => {
   const form = document.getElementById('add-card-form')
-  form.style.display = 'block'
+  form.style.display = 'flex'
   const addButton = document.querySelector('.add-card')
   addButton.style.display = 'none'
+
+  // if the user clicks on anywhere outside the form, close it
+  window.onclick = function (event) {
+    if (event.target == form) {
+      form.style.display = 'none'
+      addButton.style.display = 'flex'
+    }
+  }
 })
+
+const submitButton = document.getElementById('submit')
+submitButton.addEventListener('click', addBookFromForm)
